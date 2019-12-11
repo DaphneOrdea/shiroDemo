d70f20dcf51c0fc0626c7c86cfb6bc42dd89f91d
phantom <1342148007@qq.com>
2019-12-11 11:50:34 +0800

    初始例子提交

--- shiro-01/src/main/java/com/phantom/App.java
+++ shiro-01/src/main/java/com/phantom/App.java
@@ -1,13 +1,43 @@
 package com.phantom;
 
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.config.IniSecurityManagerFactory;
+import org.apache.shiro.mgt.DefaultSecurityManager;
+import org.apache.shiro.realm.text.IniRealm;
+import org.apache.shiro.subject.Subject;
+
 /**
  * Hello world!
- *
+ *  shiro 的流程配置
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        //创建默认的securityManager（安全管理器）
+        DefaultSecurityManager defaultSecurityManager = new DefaultSecurityManager();
+        //创建Ini 域。读取shiro.ini文件
+        IniRealm iniRealm = new IniRealm("classpath:shiro.ini");
+        //读取域
+        defaultSecurityManager.setRealm(iniRealm);
+        //将securityManager对象设置到运行环境中
+        SecurityUtils.setSecurityManager(defaultSecurityManager);
+        //通过SecurityUtils获取主体subject(主体)
+        Subject subject = SecurityUtils.getSubject();
+        //这里的username和password表示用户登陆时的账户密码，而非域里面查找的username和password。Token(凭证)
+        UsernamePasswordToken usernamePasswordToken = new UsernamePasswordToken("DaphneOrdea", "yyna");
+        try {
+            //进行用户身份认证
+            subject.login(usernamePasswordToken);
+            //验证用户是否验证成功。Authenticated(认证)
+            if (subject.isAuthenticated()) {
+                System.out.println("用户登陆成功");
+            }
+        } catch (AuthenticationException e) {
+            e.printStackTrace();
+            System.out.println("用户名或密码认证失败");
+        }
     }
 }
