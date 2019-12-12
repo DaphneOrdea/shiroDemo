efb002f5dec6721ff228a7d3414796733f892c94
phantom <1342148007@qq.com>
2019-12-12 09:34:18 +0800

    修改jdbc-配置,使用编程式，可以使用验证

--- shiro-jdbc/src/main/java/com/phantom/App.java
+++ shiro-jdbc/src/main/java/com/phantom/App.java
@@ -5,6 +5,7 @@ import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.mgt.DefaultSecurityManager;
+import org.apache.shiro.realm.jdbc.JdbcRealm;
 import org.apache.shiro.realm.text.IniRealm;
 import org.apache.shiro.subject.Subject;
 
@@ -17,6 +18,7 @@ public class App
 {
     public static void main( String[] args )
     {
+
         //创建默认的安全管理器
         DefaultSecurityManager securityManager = new DefaultSecurityManager();
         IniRealm iniRealm = new IniRealm("classpath:shiro.ini");
@@ -32,5 +34,24 @@ public class App
         } catch (AuthenticationException e) {
             e.printStackTrace();
         }
+//        DefaultSecurityManager defaultSecurityManager = new DefaultSecurityManager();
+//        JdbcRealm jdbcRealm = new JdbcRealm();
+//        DruidDataSource druidDataSource = new DruidDataSource();
+//        druidDataSource.setDriverClassName("com.mysql.jdbc.Driver");
+//        druidDataSource.setUsername("root");
+//        druidDataSource.setPassword("root");
+//        druidDataSource.setUrl("jdbc:mysql://127.0.0.1:3306/shiro?useUnicode=true&characterEncoding=UTF-8&serverTimezone=UTC&useSSL=false");
+//        jdbcRealm.setDataSource(druidDataSource);
+//        defaultSecurityManager.setRealm(jdbcRealm);
+//        SecurityUtils.setSecurityManager(defaultSecurityManager);
+//        Subject subject = SecurityUtils.getSubject();
+//        try {
+//            subject.login(new UsernamePasswordToken("张三","12345"));
+//            if (subject.isAuthenticated()){
+//                System.out.println("验证通过");
+//            }
+//        } catch (AuthenticationException e) {
+//            e.printStackTrace();
+//        }
     }
 }
